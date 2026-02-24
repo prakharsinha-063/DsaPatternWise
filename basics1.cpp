fabe44fa37f32b543c27a5a0ae84232089cf888a
prakharsinha-063 <167365919+prakharsinha-063@users.noreply.github.com>
2026-02-24 23:29:54 +0530

    basics of c++

---
+++ basics1.cpp
@@ -0,0 +1,39 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//   //input
+//   int a,b;
+//   cin>>a>>b; 
+//   //output
+//   cout<<"The value of a is "<<a<<endl;
+//   cout<<"The value of b is "<<b<<endl;
+//   //variables
+//   int age=23;
+//   cout<<"your age is "<<age<<endl;
+//   //operations
+//   cout<<"sum"<<a+b<<endl;
+//   cout<<"difference"<<a-b<<endl;
+//   cout<<"product"<<a*b<<endl;
+//   cout<<"quotient "<<a/b<<endl;
+//   cout<<"remainder"<<a%b<<endl;
+// }
+
+//product of 3 numbers
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//   int a,b,c;
+//   cout<<"enter the values of a,b and c";
+//   cin>>a>>b>>c;
+//   cout<<"the product of a,b and c is "<<a*b*c;
+// }
+
+//average of 2 numbers
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int a,b;
+  cin>>a>>b;
+  cout<<"average is "<<float((a+b)/2.0);
+  return 0;
+}
